245db1fba48f4fd0388bf65664c53029cfe26340
saa27 <saakshi27050@gmail.com>
2020-05-31 01:07:30 +0530

    Input Form

--- screens/NewPlaceScreen.js
+++ screens/NewPlaceScreen.js
@@ -1,11 +1,30 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Button,
+  TextInput,
+  ScrollView,
+} from "react-native";
+
+import Colors from "../constants/Colors";
 
 const NewPlaceScreen = (props) => {
+  const [title, setTitle] = useState("");
+
+  const titleChangeHandler = (text) => {
+    setTitle(text);
+  };
+
   return (
-    <View>
-      <Text>Places List Screen!</Text>
-    </View>
+    <ScrollView>
+      <View style={styles.form}>
+        <Text style={styles.label}>Title</Text>
+        <TextInput style={styles.textInput} onChangeText={titleChangeHandler} value={title} />
+        <Button title="Save Place" color={Colors.primary} onPress={() => {}} />
+      </View>
+    </ScrollView>
   );
 };
 
@@ -13,6 +32,21 @@ NewPlaceScreen.navigationOptions = {
   headerTitle: "Add Place",
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  label: {
+    fontSize: 18,
+    marginBottom: 15,
+  },
+  form: {
+    margin: 30,
+  },
+  textInput: {
+    borderBottomColor: "#ccc",
+    borderBottomWidth: 1,
+    marginBottom: 15,
+    paddingVertical: 4,
+    paddingHorizontal: 2,
+  },
+});
 
 export default NewPlaceScreen;
